d2f7d7a79a0d14ea2707f3e745f381174c52c58a
ElChupes <135674846+ElChupes@users.noreply.github.com>
2023-06-05 22:53:04 -0300

    primercommit

---
+++ index.js
@@ -0,0 +1,7 @@
+const miBoton = document.getElementById("miBoton");
+function handleClick(){
+    alert ("¡Vamos Boca!");
+}
+miBoton.addEventListener ("click", function("Hola"){
+    alert("Hiciste ClicK en el boton");
+(};
